414b7d31906a0596c314e7519fe6c08e5ac66d3a
iamtash <natashachernis@gmail.com>
2019-06-18 21:31:50 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, rand.to_s
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[params.keys.first] = params.values.first
+        @session = session
+
+        erb :show
+    end
 end
